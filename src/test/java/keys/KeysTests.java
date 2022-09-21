acca7308681b53e5471b88e463f331f48ad690d9
Lora11Lora22 <larisa345274578@gmail.com>
2022-09-21 19:19:56 +0300

    homework 6

---
+++ src/test/java/keys/KeysTests.java
@@ -0,0 +1,27 @@
+package keys;
+
+import base.BaseTests;
+import org.openqa.selenium.Keys;
+import org.testng.annotations.Test;
+import pages.KeyPressesPage;
+
+import static org.testng.Assert.assertEquals;
+
+public class KeysTests extends BaseTests {
+
+    private KeyPressesPage keyPage;
+
+    @Test
+    public void testBackspace(){
+        keyPage = homePage.clickKeyPresses();
+        keyPage.enterText("A" + Keys.BACK_SPACE);
+        assertEquals(keyPage.getResult(),"You entered: BACK_SPACE");
+    }
+
+    @Test
+    public void testPi(){
+//        KeyPressesPage keyPage = homePage.clickKeyPresses();
+        keyPage.enterPi();
+    }
+}
+
