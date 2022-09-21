dcc673c5dbdc7faf2f184226ef0bc771f05b7936
Lora11Lora22 <larisa345274578@gmail.com>
2022-09-21 19:00:25 +0300

    done

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # New-Project
+mvn clean
+mvn test
+mvn clean test
---
+++ src/test/java/base/HomePage.java
@@ -0,0 +1,27 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class HomePage {
+
+    private WebDriver driver;
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public KeyPressesPage clickKeyPresses() {
+        clickLink("Key Presses");
+        return new KeyPressesPage(driver);
+    }
+
+    public HorizontalSliderPage clickHorizontalSlider() {
+        clickLink("Horizontal Slider");
+        return new HorizontalSliderPage(driver);
+    }
+
+    private void clickLink(String linkText) {
+        driver.findElement(By.linkText(linkText)).click();
+    }
+}
